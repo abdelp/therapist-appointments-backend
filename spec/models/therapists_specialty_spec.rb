1b2fb07ce1f0317c9b6fa7c956cecc573008be76
abdelp <juniorperezpy@gmail.com>
2020-10-20 12:46:33 -0300

    Add TherapistsSpecialty specs

---
+++ spec/models/therapists_specialty_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe TherapistsSpecialty, type: :model do
+  it { should belong_to(:user) }
+  it { should belong_to(:specialty) }
+  it { should validate_presence_of(:user) }
+  it { should validate_presence_of(:specialty) }
+end
