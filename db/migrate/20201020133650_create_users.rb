d11bba777a3d7ccc0863dd556f7102b20256c031
abdelp <juniorperezpy@gmail.com>
2020-10-20 11:15:30 -0300

    Add user migration

---
+++ db/migrate/20201020133650_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.text :email, null: false
+
+      t.timestamps
+    end
+
+    add_index :users, :username, unique: true
+    add_index :users, :email, unique: true
+  end
+end
