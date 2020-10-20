67908e618c78a562c1465e7964027ba7939a8be5
abdelp <juniorperezpy@gmail.com>
2020-10-20 12:48:32 -0300

    Add association with therapist_specialty

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { should validate_presence_of(:username) }
   it { should validate_presence_of(:email) }
+  it { should have_many(:therapist_specialty).dependent(:destroy) }
 end
