d82612c58bfaeaa3d96d2a89b535bf249b926012
abdelp <juniorperezpy@gmail.com>
2020-10-20 13:31:37 -0300

    Add Appointment specs

---
+++ spec/models/appointment_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Appointment, type: :model do
+  it { should belong_to(:user) }
+  it { should belong_to(:therapist) }
+  it { should validate_presence_of(:start_date) }
+  it { should validate_presence_of(:end_date) }
+end
