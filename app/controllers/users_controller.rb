562a2f119d1a5928cd8963b058c4824979697e39
abdelp <juniorperezpy@gmail.com>
2020-10-20 17:35:58 -0300

    Add create, show, update, destroy methods on User Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,30 @@ class UsersController < ApplicationController
     json_response(@users)
   end
 
+  def create
+    @user = User.create!(user_params)
+    json_response(@user, :created)
+  end
+
+  def show
+    json_response(@user)
+  end
+
+  def update
+    @user.update(user_params)
+    head :no_content
+  end
+
+  def destroy
+    @user.destroy
+    head :no_content
+  end
+
   private
+
+  def user_params
+    params.permit(:username, :email)
+  end
 
   def set_user
     @user = User.find(params[:id])
