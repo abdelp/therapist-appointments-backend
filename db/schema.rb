a1e2e08c4920811aeb3ef52f147efe3c8d28685c
abdelp <juniorperezpy@gmail.com>
2020-10-20 19:11:25 -0300

    Add unique constraint to therapists user_id

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,18 @@
-ActiveRecord::Schema.define(version: 2020_10_20_161124) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2020_10_20_220511) do
+
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "appointments", force: :cascade do |t|
@@ -25,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_10_20_161124) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "idx_uk_therapists_on_user_id", unique: true
     t.index ["user_id"], name: "index_therapists_on_user_id"
   end
 
