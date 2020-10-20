8353615e4a2e02c01ca8aa70d85b07df25a9f533
abdelp <juniorperezpy@gmail.com>
2020-10-20 15:17:21 -0300

    Add index action for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+  
+  def index
+    @users = User.all
+    json_response(@users)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
