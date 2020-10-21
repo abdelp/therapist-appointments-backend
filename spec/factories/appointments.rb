57a60add16b9792c12889fae82b63d7c70b819d3
abdelp <juniorperezpy@gmail.com>
2020-10-21 18:38:20 -0300

    Fix therapist id factory

--- spec/factories/appointments.rb
+++ spec/factories/appointments.rb
@@ -1,17 +1,17 @@
 FactoryBot.define do
   factory :appointment do
     trait :user_id do
-      user_id { nil }    
+      user_id { nil }
     end
-  
+
     trait :therapist_id do
-      user_id { nil }    
+      therapist_id { nil }
     end
 
     start_at { Faker::Time.between(from: 2.years.ago, to: 2.years.since) }
     end_at { start_at + rand(15..180).minutes }
     user_id nil
-    therapist_id nil
+    therapist_id { nil }
 
     trait :in_one_hour do
       end_at { start_at + 1.hour }
