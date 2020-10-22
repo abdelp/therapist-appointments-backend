e2dbe23eadca33ff5583fb805aa945d7e926ac68
abdelp <juniorperezpy@gmail.com>
2020-10-22 11:16:41 -0300

    Add therapist controller actions

--- app/controllers/therapists_controller.rb
+++ app/controllers/therapists_controller.rb
@@ -1,2 +1,37 @@
 class TherapistsController < ApplicationController
+  before_action :set_therapist, only: [:show, :update, :destroy]
+  
+  def index
+    @therapists = Therapist.all
+    json_response(@therapists)
+  end
+
+  def create
+    @therapist = Therapist.create!(therapist_params)
+    json_response(@therapist, :created)
+  end
+
+  def show
+    json_response(@therapist)
+  end
+
+  def update
+    @therapist.update(therapist_params)
+    head :no_content
+  end
+
+  def destroy
+    @therapist.destroy
+    head :no_content
+  end
+
+  private
+
+  def therapist_params
+    params.permit(:user_id)
+  end
+
+  def set_therapist
+    @therapist = Therapist.find(params[:id])
+  end
 end
