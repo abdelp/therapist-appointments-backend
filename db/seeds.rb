7d28076e1272981998b1775a1102ba012e27acc4
abdelp <juniorperezpy@gmail.com>
2020-10-22 18:24:49 -0300

    Add seed for user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(username: 'abdelp5', email: 'juniorperezpy5@gmail.com', password: '123456')
