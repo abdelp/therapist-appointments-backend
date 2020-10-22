5f9658709e275349bdf619b07dc20aeb647619e2
abdelp <juniorperezpy@gmail.com>
2020-10-22 13:25:49 -0300

    Add login and auto_login routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     resources :appointments
   end
 
+  post '/login', to: 'users#login'
+  get '/auto_login', to: 'users#auto_login'
+
   resources :therapists do
     resources :appointments
   end
