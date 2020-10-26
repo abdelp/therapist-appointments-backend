457d63bc465d69b0b87bbadde572153a2012b562
abdelp <juniorperezpy@gmail.com>
2020-10-26 15:18:28 -0300

    Add fullname to user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.create(username: 'abdelp5', email: 'juniorperezpy5@gmail.com', password: '123456')
+user = User.create(username: 'abdelp5', email: 'juniorperezpy5@gmail.com', fullname: 'Abdel Perez', password: '123456')
