72f1c8062b73731b7dc8b4ee9d8d9e86682b0196
abdelp <juniorperezpy@gmail.com>
2020-10-27 13:38:23 -0300

    Add img_url to therapists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_26_173507) do
+ActiveRecord::Schema.define(version: 2020_10_27_161007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_10_26_173507) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "img_url"
     t.index ["user_id"], name: "idx_uk_therapists_on_user_id", unique: true
     t.index ["user_id"], name: "index_therapists_on_user_id"
   end
