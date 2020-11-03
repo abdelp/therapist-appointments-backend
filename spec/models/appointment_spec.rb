02548cbc65faa27ee746062bd74f03e928edf4d1
abdelp <juniorperezpy@gmail.com>
2020-11-03 19:53:11 -0300

    Remove specialties Rspecs

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -3,6 +3,5 @@ require 'rails_helper'
 RSpec.describe Appointment, type: :model do
   it { should belong_to(:user) }
   it { should belong_to(:therapist) }
-  it { should validate_presence_of(:start_date) }
-  it { should validate_presence_of(:end_date) }
+  it { should validate_presence_of(:start_at) }
 end
--- spec/models/specialty_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Specialty, type: :model do
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:description) }
-  it { should have_many(:therapist_specialty).dependent(:destroy) }
-end
