0ed837bb9697f93df806ca612d579a2c7ce6c7f5
abdelp <juniorperezpy@gmail.com>
2020-11-04 20:35:39 -0300

    Fix rubocop offenses

--- spec/factories/therapists.rb
+++ spec/factories/therapists.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :therapist do
-    sequence(:user_id) { |n|
+    sequence(:user_id) do |n|
       n
-    }
+    end
     img_url { '' }
     created_at { '2020-01-01' }
     updated_at { '2020-01-01' }
