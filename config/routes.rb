3046fabfd35a6857df328531f1496b485d5504e4
abdelp <juniorperezpy@gmail.com>
2020-11-07 11:07:18 -0300

    Remove non namespaced routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,4 @@
 Rails.application.routes.draw do
-  # resources :users do
-  #   resources :appointments
-  # end
-
-  # post '/login', to: 'users#login'
-  # get '/auto_login', to: 'users#auto_login'
-
-  # resources :therapists do
-  #   resources :appointments
-  # end
-
   namespace :api, :path => "", :constraints => {:subdomain => "api"} do
     namespace :v1 do
       resources :users do
